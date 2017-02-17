b9d0faac9f5feae5f1e3eaefe5d62d186f184b7f
Getro Naissance <getro89@gmail.com>
2017-02-17 13:07:37 -0800

    it will add the corresponding views for the following actions, inde, show, edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,19 @@
 class ProductsController < ApplicationController
   def index
-
+    @products = Product.all
   end
 
   def show
-
   end
 
   def new
-
+    @product = Product.new
   end
 
   def edit
-
   end
 
   def create
-
   end
 
   def update
@@ -24,7 +21,6 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-
   end
 
 private
